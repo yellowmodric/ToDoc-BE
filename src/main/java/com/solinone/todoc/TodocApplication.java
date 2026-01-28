acc64518cbd3509d99c7d3d8284185e0ff785ff1
yellowmodric <kyr7091@naver.com>
2026-01-28 16:51:12 +0900

    chore: 개발환경 세팅

---
+++ src/main/java/com/solinone/todoc/TodocApplication.java
@@ -0,0 +1,13 @@
+package com.solinone.todoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solinone/todoc/TodocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solinone.todoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
