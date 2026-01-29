bdeb6e68df366160c38c5d9ddca526b52b730f6e
haejun <162435572+HaejunJang@users.noreply.github.com>
2026-01-29 17:16:02 +0900

    chore: 공통 응답 포맷 추가

---
+++ src/main/java/com/solinone/todoc/global/exception/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.solinone.todoc.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    //common (C)
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C001", "서버에서 알 수 없는 오류가 발생했습니다"),
+    INVALID_ARGUMENT_TYPE(HttpStatus.BAD_REQUEST, "C002", "요청 값 타입이 잘못되었습니다"),
+    MISSING_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST, "C003", "요청 값이 누락되었습니다"),
+    INVALID_REQUEST_VALUE(HttpStatus.BAD_REQUEST, "C004", "요청 값이 잘못되었습니다"),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "C005", "접근 권한이 없습니다");
+    //user (U)
+
+    //place (P)
+
+    private final HttpStatus httpStatus;
+    private final String errorCode;
+    private final String message;
+}
