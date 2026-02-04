923b9c27e9844f7fc6f2a148364f246eb58a2f05
haejun <162435572+HaejunJang@users.noreply.github.com>
2026-02-04 17:10:16 +0900

    fix: 공통 응답 포맷 수정

---
+++ src/main/java/com/solinone/todoc/global/response/MessageResponse.java
@@ -0,0 +1,10 @@
+package com.solinone.todoc.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
