db257ce04aa7de318ae646249ebbd81e883e3672
yellowmodric <kyr7091@naver.com>
2026-02-05 11:55:54 +0900

    feat: 내 가게 목록 조회

---
+++ src/main/java/com/solinone/todoc/place/dto/response/PlaceResponse.java
@@ -0,0 +1,11 @@
+package com.solinone.todoc.place.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PlaceResponse {
+    private Long placeId;
+    private String placeName;
+}
--- src/main/java/com/solinone/todoc/place/infrastructure/PlaceRepository.java
+++ src/main/java/com/solinone/todoc/place/infrastructure/PlaceRepository.java
@@ -3,6 +3,9 @@ package com.solinone.todoc.place.infrastructure;
 import com.solinone.todoc.place.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlaceRepository extends JpaRepository<Place, Long> {
     boolean existsByBusinessNumberAndAddress(String businessNumber, String address);
+    List<Place> findByUser_UserIdOrderByCreatedAtDesc(Long userId);
 }
