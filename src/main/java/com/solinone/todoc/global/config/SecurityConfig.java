3db8c856cc6f3dd26b051ae99e13864e6514ee02
yellowmodric <kyr7091@naver.com>
2026-02-09 16:00:37 +0900

    hotfix: securityConfig 수정

--- src/main/java/com/solinone/todoc/global/config/SecurityConfig.java
+++ src/main/java/com/solinone/todoc/global/config/SecurityConfig.java
@@ -37,8 +37,8 @@ public class SecurityConfig {
                         -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/users/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**",
-                                "/v3/api-docs"
+                                "/api/users/**", "/api/swagger-ui/**", "/api/swagger-ui.html", "/api/v3/api-docs/**",
+                                "/api/v3/api-docs"
                         ).permitAll()
                         .anyRequest().permitAll()
                 )
