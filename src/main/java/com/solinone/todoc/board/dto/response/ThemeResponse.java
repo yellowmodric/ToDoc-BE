2028afe147c74abdb7e547cfc5c00f1a5877507d
haejun <162435572+HaejunJang@users.noreply.github.com>
2026-02-11 17:50:07 +0900

    fix: 테마조회 응답 수정

--- src/main/java/com/solinone/todoc/board/dto/response/ThemeResponse.java
+++ src/main/java/com/solinone/todoc/board/dto/response/ThemeResponse.java
@@ -14,6 +14,7 @@ public class ThemeResponse {
     private Long themeId;
     private String themeName;
     private List<String> tags;
+    private List<String> themeUrls;
 
     public static ThemeResponse from(Theme theme) {
         List<String> tags = theme.getThemeTags().stream()
@@ -23,7 +24,23 @@ public class ThemeResponse {
         return ThemeResponse.builder()
                 .themeId(theme.getThemeId())
                 .themeName(theme.getThemeName())
+                .themeUrls(generateThemeUrls(theme.getThemeId()))
                 .tags(tags)
                 .build();
+    }
+
+    private static List<String> generateThemeUrls(Long themeId) {
+        String baseUrl = "https://todocbucket.s3.ap-northeast-2.amazonaws.com/theme/";
+
+        if (themeId == 5) {
+            return List.of(baseUrl + "5/1.png");
+        }
+
+        return List.of(
+                baseUrl + themeId + "/1.png",
+                baseUrl + themeId + "/2.png",
+                baseUrl + themeId + "/3.png",
+                baseUrl + themeId + "/4.png"
+        );
     }
 }
